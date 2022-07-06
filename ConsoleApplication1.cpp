23b2d4bb240370bb84e436198bf134f7c031cf17
Norgonka <82911658+Norgonka@users.noreply.github.com>
2022-07-06 14:36:51 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+#include <ctime>
+#include <string>
+using namespace std;
+int Strlen(string a)
+{
+    int sum = 0;
+    for (; a[sum] != '\0'; sum++)
+    {
+
+    }
+    return sum;
+}
+int SumOfPoints(string a)
+{
+    int sum = 0;
+    for (int i = 0; i < Strlen(a); i++)
+    {
+        if (a[i] == 'e' || a[i] == 'a' || a[i] == 'i' || a[i] == 'o' || a[i] == 'n' || a[i] == 'r' || a[i] == 't' || a[i] == 'l' || a[i] == 's' || a[i] == 'u')
+        {
+            sum++;
+        }
+        else if (a[i] == 'd' || a[i] == 'g')
+        {
+            sum += 2;
+        }
+        else if (a[i] == 'b' || a[i] == 'c' || a[i] == 'm' || a[i] == 'p')
+        {
+            sum += 3;
+        }
+        else if (a[i] == 'f' || a[i] == 'h' || a[i] == 'v' || a[i] == 'w' || a[i] == 'y')
+        {
+            sum += 4;
+        }
+        else if (a[i] == 'k')
+        {
+            sum += 5;
+        }
+        else if (a[i] == 'j' || a[i] == 'x')
+        {
+            sum += 8;
+        }
+        else if (a[i] == 'q' || a[i] == 'z')
+        {
+            sum += 10;
+        }
+    }
+    return sum;
+}
+
+int main()
+{
+    int n;
+    cin >> n; cin.ignore();
+    string correctword = "";
+    string bestword = "";
+    int sum = 0;
+    int bestsum = 0;
+    string* allwords = new string[n];
+    for (int i = 0; i < n; i++)
+    {
+        string w;
+        getline(cin, w);
+        allwords[i] = w;
+    }
+    string letters;
+    getline(cin, letters);
+    for (int i = 0; i < n; i++)
+    {
+        string word = allwords[i];
+        int a = 0;
+        string test = letters;
+        for (int j = 0; j < Strlen(letters); j++)
+        {
+
+            if (word[a] == test[j])
+            {
+                a++;
+                test[j] = '@';
+                j = -1;
+            }
+            if (a == Strlen(word))
+            {
+                correctword = word;
+                sum = SumOfPoints(correctword);
+                if (sum > bestsum)
+                {
+                    bestword = correctword;
+                }
+            }
+        }
+    }
+    delete[] allwords;
+    allwords = nullptr;
+    cout << bestword << endl;
+}
---
+++ Ya Sdelyal.cpp
@@ -0,0 +1,130 @@
+﻿#include <iostream>
+using namespace std;
+class MyString
+{
+public:
+	MyString()
+	{
+		str = nullptr;
+	};
+	MyString(const char* str) 
+	{
+		this->length = strlen(str);
+		this->str = new char[this->length + 1];
+		for (int i = 0; i < this->length; i++)
+		{
+			this->str[i] = str[i];
+		}
+		this->str[this->length] = '\0';
+	};
+	~MyString()
+	{
+		delete[] this->str;
+		this->str = nullptr;
+	};
+	MyString(const MyString& other)
+	{
+		
+		int length = strlen(other.str);
+		this->str = new char[length + 1];
+		for (int i = 0; i < length; i++)
+		{
+			this->str[i] = other.str[i];
+		}
+		this->str[length] = '\0';
+	}
+	MyString &operator =(const MyString &other)
+	{
+		if (this->str != nullptr)
+		{
+			delete[] str;
+		}
+		int length = strlen(other.str);
+		this->str = new char[length + 1];
+		for (int i = 0; i < length; i++)
+		{
+			this->str[i] = other.str[i];
+		}
+		this->str[length] = '\0';
+		return *this;
+	}
+	MyString operator+(const MyString& other)
+	{
+		MyString result;
+		int length1 = strlen(this->str);
+		int length2 = strlen(other.str);
+		int length3 = length1 + length2;
+		int k = 0;
+		result.str = new char[length3 + 1];
+		for (int i = 0; i < length1; i++)
+		{
+			result.str[i] = this->str[i];
+		}
+		for (int j = length1; j < length3; j++, k++)
+		{
+			result.str[j] = other.str[k];
+		}
+		result.str[length3] = '\0';
+		return result;
+	}
+	int	Length()
+	{
+		return this->length;
+	}
+	bool operator ==(const MyString &other)
+	{
+		bool result = this->length == other.length ? true : false;
+		if (result == true)
+		{
+			for (int i = 0; i < length; i++)
+			{
+				result = this->str[i] == other.str[i];
+				if (result == false)
+				{
+					break;
+				}
+			}
+		}
+		return result;
+	}
+	bool operator !=(const MyString& other)
+	{
+		bool result = this->length == other.length ? true : false;
+		if (result == true)
+		{
+			for (int i = 0; i < length; i++)
+			{
+				result = this->str[i] == other.str[i];
+				if (result == false)
+				{
+					break;
+				}
+			}
+		}
+		return (result == false) ? true : false;
+	}
+	char &operator[](int i)
+	{
+		return this->str[i];
+	}
+	void Print()
+	{
+		for (int i = 0; i < this->length; i++)
+		{
+			cout << this->str[i];
+		}
+	}
+private:
+	char* str;
+	int length;
+};
+int main()
+{
+	MyString str("Hello");
+	MyString str2("Hello");
+	str[2] = 'e';
+	str.Print();
+	return 0;
+}
+
+
